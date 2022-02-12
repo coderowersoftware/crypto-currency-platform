8bf65019a011a9b49825459262a4512be23f5ecd
hem-tech01 <hem.tech01@gmail.com>
2022-02-12 16:47:10 +0530

    updating contract

--- src/Transactions/Controllers/Models/Reports/OverallLicenseDetails.cs
+++ src/Transactions/Controllers/Models/Reports/OverallLicenseDetails.cs
@@ -14,5 +14,8 @@ namespace CodeRower.CCP.Controllers.Models.Reports
         public decimal CoinsInFarming { get; set; }
         public decimal CoinsInMinting { get; set; }
         public decimal CoinsInWallet { get; set; }
+        public int LicenseUsers { get; internal set; }
+        public int PoolLicenseMiners { get; internal set; }
+        public int ActiveLicenseMiners { get; internal set; }
     }
 }
